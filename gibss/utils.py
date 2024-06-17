49d24d8270c8492cf984bb3008961ab61585a675
Karl Tayeb <karl.tayeb7@gmail.com>
2024-06-17 12:55:28 -0500

    function to convert to dict of numpy arrays for interface with R

---
+++ gibss/utils.py
@@ -0,0 +1,15 @@
+
+import dataclasses
+import jax
+import numpy as np
+
+def todict(x):
+    # if its a dataclass, convert to dict
+    if dataclasses.is_dataclass(x):
+        x = dataclasses.asdict(x)
+    # numpy-ize the pytree
+    x = jax.tree.map(lambda x: np.array(x) if isinstance(x, jax.Array) else x, x)
+
+    # NOTE: eventually we should improve monitor and report information about model fitting
+    monitor = x.pop('monitor', None)
+    return x
