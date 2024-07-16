420a0364fcbbff691fb36f613a83cc93d90b42d9
Karl Tayeb <karl.tayeb7@gmail.com>
2024-07-16 16:32:14 -0500

    seperate ser computation from parallel fitting step

--- gibss/ser.py
+++ gibss/ser.py
@@ -15,18 +15,19 @@ class SER:
     fits: Any  # parameters
     prior_variance: float
 
-# @partial(jax.jit, static_argnames=['fitter', 'nullfitter'])
-def ser(coef_init, X, y, offset, prior_variance, fitter, nullfitter):
+
+def _ser(fits, prior_variance, X):
     """Logistic SER"""
-    # fit logistic regression for each column of X
-    x0 = jnp.zeros_like(y)
-    nullfit = nullfitter(y, offset)
-    fits = fitter(coef_init, X, y, offset, prior_variance, nullfit)
     alpha = jnp.exp(fits.lbf - jax.scipy.special.logsumexp(fits.lbf))
-    lbf_ser = jax.scipy.special.logsumexp(fits.lbf - jnp.log(X.shape[0]))
+    lbf_ser = jax.scipy.special.logsumexp(fits.lbf - jnp.log(fits.lbf.size))
     # compute predictions
     # note the predictions do not include the contribution of the intercept
     psi = X.T @ (alpha * fits.beta)
-    
-    # return coef
     return SER(psi, alpha, lbf_ser, fits, prior_variance)
+
+
+def ser(coef_init, X, y, offset, prior_variance, fitter, nullfitter):
+    """Logistic SER"""
+    nullfit = nullfitter(y, offset)
+    fits = fitter(coef_init, X, y, offset, prior_variance, nullfit)
+    return _ser(fits, prior_variance, X)
