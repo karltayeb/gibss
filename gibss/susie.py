2ef2ca6e3165625d13d5d3b5ef57cb6ce367267a
Karl Tayeb <karl.tayeb7@gmail.com>
2025-01-04 20:32:04 -0600

    add option to keep intermediate values

--- gibss/susie.py
+++ gibss/susie.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
-from typing import Any, Callable
+from typing import Any, Callable, List
 from functools import partial
 import numpy as np
 from scipy import sparse
 from gibss.gibss import gibss
 import jax
 import jax.numpy as jnp
+from jax import Array
+from gibss.credible_sets import compute_cs, CS
 
 @partial(jax.tree_util.register_dataclass,
          data_fields=['tol', 'converged', 'maxiter', 'iter'], meta_fields=[])
@@ -31,6 +33,12 @@ def check_not_converged(val):
             lambda: True # stay in the while loop
     )
 
+@partial(jax.tree_util.register_dataclass,
+         data_fields=['psi', 'fit'], meta_fields=[])
+@dataclass
+class AdditiveComponent:
+    psi: Array  # predictions
+    fit: Any  # parameters
 
 def ensure_dense_and_float(matrix):
     """
@@ -68,7 +76,16 @@ def make_ser_fitfun(X: np.ndarray, y: np.ndarray, serfun: Callable, initfun: Cal
             X = X, y=y, offset = psi,
             **serkwargs
         )
-    return serfitfun
+
+    @jax.jit
+    def serfitfun2(psi, fit):
+        coef_init = fit.fits.state.x
+        return serfun(
+            coef_init = coef_init,
+            X = X, y=y, offset = psi,
+            **serkwargs
+        )
+    return serfitfun, serfitfun2
 
 
 def fit_susie_jax(X: np.ndarray, y: np.ndarray, L: int, serfun: Callable, initfun: Callable, serkwargs: dict, tol=1e-3, maxiter=10) -> Any:
@@ -121,7 +138,7 @@ def tree_unstack(tree):
     leaves, treedef = jax.tree.flatten(tree)
     return [treedef.unflatten(leaf) for leaf in zip(*leaves, strict=True)]
     
-def fit_susie(X: np.ndarray, y: np.ndarray, L: int, serfun: Callable, initfun: Callable, serkwargs: dict, tol=1e-3, maxiter=10) -> Any:
+def fit_susie(X: np.ndarray, y: np.ndarray, serfun: Callable, initfun: Callable, serkwargs: dict, L: int=5, tol: float=1e-3, maxiter: int=10, warm_init: bool=False, cleanup: bool = False, keep_intermediate=False) -> Any:
     # get the right types
     X = ensure_dense_and_float(X)
     y = ensure_dense_and_float(y)
@@ -130,16 +147,19 @@ def fit_susie(X: np.ndarray, y: np.ndarray, L: int, serfun: Callable, initfun: C
     tol = float(tol)
 
     # initialization
-    fitfun = make_ser_fitfun(X, y, serfun, initfun, serkwargs)
+    fitfun, fitfun2 = make_ser_fitfun(X, y, serfun, initfun, serkwargs)
     psi_init = jnp.zeros_like(y).astype(float)
     psi = jnp.zeros_like(y)
     components = []
-
+    intermediate = []
     # forard selection
     for l in range(L):
         component = fitfun(psi)
         psi = psi + component.psi
         components.append(component)
+    state = AdditiveState(tol, False, maxiter, 1)
+    if keep_intermediate:
+        intermediate.append(tree_stack(components))
 
     # iterative stepwise selection
     i = 0
@@ -148,29 +168,82 @@ def fit_susie(X: np.ndarray, y: np.ndarray, L: int, serfun: Callable, initfun: C
         psi_old = psi
         for l in range(L):
             psi = psi - components[l].psi
-            components[l] = fitfun(psi)
+            components[l] = fitfun2(psi, components[l]) if warm_init else fitfun(psi)
             psi = psi + components[l].psi
+        if keep_intermediate:
+            intermediate.append(tree_stack(components))
         # check convergence
         diff = jnp.abs(psi - psi_old).max()
         if diff < tol:
             break
     state = AdditiveState(tol, diff < tol, maxiter, i + 1)
     fit = tree_stack(components)
+
+    if keep_intermediate:
+        return tree_stack(intermediate), state
+
+    # optionally, format resutlts into R-friendly format
+    if cleanup:
+        fit = cleanup_susie_fit(fit, state)
     return fit, state
+
+def fit_additive(fitfuns: List[Callable], tol: float=1e-3, maxiter: int=10, keep_intermediate=False) -> Any:
+    maxiter = int(maxiter)
+    tol = float(tol)
+    L = len(fitfuns)
+    # initialization
+    psi = 0.
+    components = []
+    intermediate = []
+    # forard selection
+    for l in range(L):
+        component = fitfuns[l](psi, None)
+        components.append(component)
+        psi = psi + component.psi
+    if keep_intermediate:
+        intermediate.append(jax.tree.map(lambda x: x, components)) # makes a copy I think? Return all the same instance if we don't do this.
+    state = AdditiveState(tol, False, maxiter, 1)
+
+    # iterative stepwise selection
+    i = 0
+    diff = jnp.inf
+    for i in range(1, maxiter):
+        psi_old = psi
+        for l in range(L):
+            psi = psi - components[l].psi
+            components[l] = fitfuns[l](psi, components[l])
+            psi = psi + components[l].psi
+        if keep_intermediate:
+            intermediate.append(jax.tree.map(lambda x: x, components))
+        diff = jnp.abs(psi - psi_old).max()
+        state = AdditiveState(tol, diff < tol, maxiter, i + 1)
+        if diff < tol:
+            break
+
+    if keep_intermediate:
+        return tree_stack(intermediate), state
+
+    return components, state
 
 @dataclass
 class SuSiERes:
-  alpha: np.ndarray
-  lbf: np.ndarray
-  lbf_ser: np.ndarray
-  effect: np.ndarray
-  outer_loop_state: AdditiveState
+    alpha: np.ndarray
+    lbf: np.ndarray
+    lbf_ser: np.ndarray
+    prior_variance: np.ndarray
+    effect: np.ndarray
+    outer_loop_state: AdditiveState
+    
+    @property
+    def cs(self):
+        return [compute_cs(a) for a in self.alpha]
 
 def cleanup_susie_fit(fit, state):
     return SuSiERes(
         np.array(fit.alpha),
         np.array(fit.fits.lbf),
         np.array(fit.lbf_ser),
+        np.array(fit.prior_variance),
         np.array(fit.fits.beta),
         state
     )
