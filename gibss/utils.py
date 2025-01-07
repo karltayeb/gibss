fe09c7c0a95ff1819fb012278f1a7c951c43c0cb
Karl Tayeb <karl.tayeb7@gmail.com>
2025-01-06 21:23:15 -0600

    move tree_stack, etc. to utils

--- gibss/utils.py
+++ gibss/utils.py
@@ -2,6 +2,8 @@
 import dataclasses
 import jax
 import numpy as np
+import jax.numpy as jnp
+from scipy import sparse
 
 def todict(x):
     # if its a dataclass, convert to dict
@@ -16,3 +18,39 @@ def todict(x):
         x['converged'] = monitor.converged
         x['tol'] = monitor.tol 
     return x
+
+def tree_stack(trees):
+    # https://gist.github.com/willwhitney/dd89cac6a5b771ccff18b06b33372c75
+    return jax.tree.map(lambda *v: jnp.stack(v), *trees)
+
+def tree_unstack(tree):
+    leaves, treedef = jax.tree.flatten(tree)
+    return [treedef.unflatten(leaf) for leaf in zip(*leaves, strict=True)]
+
+
+def ensure_dense_and_float(matrix):
+    """
+    X = np.random.binomial(1, np.ones((5, 5))*0.5)
+    Xsp = sparse.csr_matrix(X)
+    ensure_dense_and_float(Xsp)
+
+    y = np.random.binomial(1, np.ones(5)*0.5)
+    ensure_dense_and_float(y)
+    """
+    # Check if the input is a sparse matrix
+    if sparse.issparse(matrix):
+        # Convert sparse matrix to a dense array
+        matrix = matrix.toarray()
+        # Provide a message to the user
+        print("Input is a sparse matrix. Converting to a dense array.")
+    
+    # Ensure the matrix is a numpy array (in case it's a list or other type)
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("Input must be a sparse matrix or a numpy array.")
+    
+    # Ensure the matrix is of float type
+    if not np.issubdtype(matrix.dtype, np.floating):
+        matrix = matrix.astype(float)
+        print("Converting matrix to float type.")
+    return matrix
+
